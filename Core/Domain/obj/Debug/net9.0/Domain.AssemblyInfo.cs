77177780eb97cb71edee5159e6f5e196c31cbc8f
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2025-06-10 18:27:14 +0300

    update

--- Core/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Core/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba2336bf41c344ba46f07f544e08eccb9c6d2587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a6100e1d89acceb3ade5eb07fa2f555c04d3f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Frontend/Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Frontend/Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c002e0449950357bd48cd0f5cf1c9cb8218c19e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a6100e1d89acceb3ade5eb07fa2f555c04d3f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
