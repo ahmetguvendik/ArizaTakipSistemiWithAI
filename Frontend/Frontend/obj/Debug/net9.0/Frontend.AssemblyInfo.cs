937835b4679beeda7055018601e41fe79ac61744
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2025-06-18 17:45:51 +0300

    AddedResetPasswordInsideProfilPage

--- Frontend/Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
+++ Frontend/Frontend/obj/Debug/net9.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+121b85dfeb5a4e75cef55fae8b0005bf1ad63c92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb65d375a5acdfb26b0fc854456b909a071f388")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
